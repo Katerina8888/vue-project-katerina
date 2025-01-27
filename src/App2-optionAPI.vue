2ebde061fa8f51f7d4fdbac58baf206885a9d14d
katerina <katerina@igloonet.cz>
2025-01-27 20:01:59 +0100

    feat: option api version of App file

---
+++ src/App2-optionAPI.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li :key="task" v-for="task in tasks">{{ task }}</li>
+  </ul>
+
+  <a :href="link" target="_blink">Click for Google</a>
+  <br />
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'active',
+      tasks: ['Task One', 'Taks Two', 'Task Three'],
+      link: 'https://www.google.com/',
+    }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === 'active') {
+        this.status = 'pendign'
+      } else if (this.status === 'pending') {
+        this.status = 'inactive'
+      } else {
+        this.status = 'active'
+      }
+    },
+  },
+}
+</script>
